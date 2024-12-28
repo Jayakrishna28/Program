e29357c3b0c26d89c7874681f1988bcd369b4954
Jayakrishna28 <jayakrishnakambala3026@gmail.com>
2024-12-28 19:13:09 +0530

    All Programs

---
+++ PYTHON/Arrays.py
@@ -0,0 +1,9 @@
+heros=['spider man','thor','hulk','iron man','captain america']
+l=len(heros)
+print(l)
+heros.append("Black Panther")
+print(heros)
+heros.remove('Black Panther')
+print(heros)
+heros[2].append('BlackPanther')
+print(heros)
