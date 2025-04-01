e6801d0fdb75f16c71df2ce439cf9d8dcba942a3
Jayakrishna28 <jayakrishnakambala3026@gmail.com>
2025-04-01 18:43:32 +0530

    Some Files

---
+++ PYTHON/1.two-sum.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        
+# @lc code=end
+# @lc code=end
+
